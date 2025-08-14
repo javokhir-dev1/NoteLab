52592340e0177649a4f715a59619292a7cdb1953
javokhir-dev1 <javokhir.dev1@gmail.com>
2025-08-13 21:22:26 -0700

    text yaratish imkoniyati qoshildi

--- routes/notes.route.js
+++ routes/notes.route.js
@@ -6,9 +6,9 @@ const { Notes, noteValidationSchema } = require("../models/notes.model")
 router.post("/create-note", async (req, res) => {
     try {
         let data = await Notes.create(req.body)
-        res.send(data)
-    } catch(err) {
-        res.status(500).send({error: err.message, success: false})
+        res.send({ data, success: true })
+    } catch (err) {
+        res.status(500).send({ error: err.message, success: false })
     }
 })
 
