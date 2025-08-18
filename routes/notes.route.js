be5d613637b48795653855ad634bbfa9eb5f79d4
javokhir-dev1 <javokhir.dev1@gmail.com>
2025-08-17 20:46:36 -0700

    ozgartirishlar kiritildi

--- routes/notes.route.js
+++ routes/notes.route.js
@@ -21,5 +21,24 @@ router.post("/create-note", async (req, res) => {
     }
 })
 
+router.patch("/update-note/:id", async (req, res) => {
+    try {
+        let data = await Notes.findByIdAndUpdate(req.params.id, req.body)
+        res.send({ data, success: true })
+    } catch (err) {
+        res.status(500).send({ error: err.message, success: false })
+    }
+})
+
+router.delete("/delete-note/:id", async (req, res) => {
+    try {
+        let data = await Notes.findByIdAndDelete(req.params.id)
+        if (!data) {
+            return res.status(404).send({ error: "note not found", success: false })
+        }
+    } catch (err) {
+        res.status(500).send({ error: err.message, success: false })
+    }
+})
 
 module.exports = router
